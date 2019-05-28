59c731f32a63ea9c8acdfccd22c95aaa2ae7536c
jshidalgo <50366935+jshidalgo@users.noreply.github.com>
2019-05-28 09:32:37 -0500

    clase del 28/05/2019 con todo y el glasfhis funcionando, configuren lo del source y el pool

---
+++ herbario/negocioHerbario/src/main/java/co/gov/jsasociados/ejb/ConfiguracionEJB.java
@@ -0,0 +1,62 @@
+package co.gov.jsasociados.ejb;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import co.gov.jsasociados.Administrador;
+import co.gov.jsasociados.Cuenta;
+
+/**
+ * Se encarga de verificar la configuracion por defecto
+ */
+@Singleton
+@LocalBean
+@Startup
+public class ConfiguracionEJB {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	/**
+	 * Default constructor.
+	 */
+	public ConfiguracionEJB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * inicializar la informacion basica
+	 */
+	@PostConstruct
+	public void inicializar() {
+		// TODO Auto-generated constructor stub
+		TypedQuery<Administrador> query = entityManager.createNamedQuery(Administrador.LISTAR_ADMINISTRADORES,
+				Administrador.class);
+		List<Administrador> administradores = query.getResultList();
+		if (administradores.size() == 0) {
+			Administrador administrador = new Administrador();
+
+			administrador.setCedula("1004915534");
+			administrador.setNombre("Carlos Antonio");
+			administrador.setApellidos("Osorio Rodriguez");
+			administrador.setCorreo("correo@mail.com");
+			administrador.setDireccion("por ahi");
+
+			Cuenta cuenta = new Cuenta();
+			cuenta.setUsuario("Admin");
+			cuenta.setContrasenia("admin321");
+
+			administrador.setCuenta(cuenta);
+			
+			entityManager.persist(cuenta);
+			entityManager.persist(administrador);
+		}
+	}
+
+}
