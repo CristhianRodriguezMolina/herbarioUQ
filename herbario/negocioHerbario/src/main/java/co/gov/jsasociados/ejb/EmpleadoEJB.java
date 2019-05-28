6fef586062720083f0f6e1d4fadee8f49aad4f98
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2019-05-27 19:11:50 -0500

    EmpleadoEJB

---
+++ herbario/negocioHerbario/src/main/java/co/gov/jsasociados/ejb/EmpleadoEJB.java
@@ -0,0 +1,153 @@
+package co.gov.jsasociados.ejb;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import co.gov.jsasociados.Empleado;
+import co.gov.jsasociados.Persona;
+import co.gov.jsasociados.Recolector;
+import co.gov.jsasocioados.exeption.ElementoRepetidoException;
+import co.gov.jsasocioados.exeption.PersonaNoRegistradaException;
+import co.gov.jsasocioados.exeption.TipoClaseException;
+
+/**
+ * Session Bean implementation class EmpleadoEJB
+ */
+@Stateless
+@LocalBean
+public class EmpleadoEJB implements EmpleadoEJBRemote {
+
+	/**
+	 * permite manejar todas las transacciones manejadas por el empleado
+	 */
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+    /**
+     * Default constructor. 
+     */
+    public EmpleadoEJB() {
+    	
+    }
+
+   
+    /*
+     * (non-Javadoc)
+     * @see co.gov.jsasociados.ejb.EmpleadoEJBRemote#insertarRecolector(co.gov.jsasociados.Recolector)
+     */
+    public Recolector insertarRecolector(Recolector recolector)
+			throws ElementoRepetidoException, PersonaNoRegistradaException {
+		if (entityManager.find(Persona.class, recolector.getCedula()) != null) {
+			throw new PersonaNoRegistradaException("El recolector con esta cedula se encuentra agregado");
+		} else if (buscarPorUsuario(recolector.getCuenta().getUsuario()) != null) {
+			throw new ElementoRepetidoException("El recolector con ese usuario ya esta registrado");
+		}
+		try {
+			entityManager.persist(recolector);
+			return recolector;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+    
+    /*
+     * (non-Javadoc)
+     * @see co.gov.jsasociados.ejb.EmpleadoEJBRemote#eliminarRecolector(java.lang.String)
+     */
+    public boolean eliminarRecolector(String cedula) throws PersonaNoRegistradaException, TipoClaseException {
+		if ((entityManager.find(Persona.class, cedula)) != null) {
+			if (!((entityManager.find(Persona.class, cedula)).getClass().equals(Recolector.class))) {
+				throw new TipoClaseException("La persona que desea eliminar no es un recolector");
+			}
+		} else {
+			throw new PersonaNoRegistradaException("La persona a eliminiar no se encuentra registrada");
+		}
+		try {
+			entityManager.remove(entityManager.find(Persona.class, cedula));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+    
+    /*
+     * (non-Javadoc)
+     * @see co.gov.jsasociados.ejb.EmpleadoEJBRemote#modificarRecolector(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+     */
+    public Recolector modificarRecolector(String nombre, String apellido, String telefono, String correo,
+			String direccion, String cedula) throws PersonaNoRegistradaException, TipoClaseException {
+		Persona recolector = entityManager.find(Persona.class, cedula);
+		if (recolector == null) {
+			throw new PersonaNoRegistradaException("La persona a la que quiere modificar los datos no esta registrada");
+		} else if (!(recolector.getClass().equals(Recolector.class))) {
+			throw new TipoClaseException("La persona a la que quiere modificar sus datos no es un recolector");
+		}
+
+		// Esto previamente por interfaz debe de estar validado para que no este vacio
+		recolector.setNombre(nombre == ""? recolector.getNombre():nombre);
+		recolector.setApellidos(apellido == ""? recolector.getApellidos():apellido);
+		recolector.setTelefono(telefono == ""? recolector.getTelefono():telefono);
+		recolector.setCorreo(correo == ""? recolector.getCorreo():correo);
+		recolector.setDireccion(direccion == ""? recolector.getDireccion():direccion);
+
+		try {
+			entityManager.merge(recolector);
+			return (Recolector) recolector;
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+    
+    /*
+     * (non-Javadoc)
+     * @see co.gov.jsasociados.ejb.EmpleadoEJBRemote#listarRecolectores()
+     */
+    public List<Recolector> listarRecolectores() throws Exception {
+		try {
+			TypedQuery<Recolector> query = entityManager.createNamedQuery(Recolector.LISTAR_RECOLECTORES,
+					Recolector.class);
+			List<Recolector> recolectores = query.getResultList();
+			return recolectores;
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+    
+    /*
+     * (non-Javadoc)
+     * @see co.gov.jsasociados.ejb.EmpleadoEJBRemote#buscarRecolector(java.lang.String)
+     */
+    public Recolector buscarRecolector(String cedula) throws PersonaNoRegistradaException, TipoClaseException {
+		Persona recolector = entityManager.find(Persona.class, cedula);
+		if (recolector == null) {
+			throw new PersonaNoRegistradaException("La persona a la que quiere buscar esta registrada");
+		} else if (!(recolector.getClass().equals(Recolector.class))) {
+			throw new TipoClaseException("La persona a la que busca no es un recolector");
+		}
+		return (Recolector) recolector;
+	}
+    
+    /**
+     * Metodo para buscar una persona por su usuario
+     * @param usuario
+     * @return La persona si fue encontrada o null de lo contrario
+     */
+    private Persona buscarPorUsuario(String usuario) {
+		try {
+			TypedQuery<Persona> persona = entityManager.createNamedQuery(Persona.OBTENER_POR_USUARIO, Persona.class);
+			persona.setParameter("usuario", usuario);
+			return (Empleado) persona.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+
+	}
+}
---
+++ herbario/negocioHerbario/src/main/java/co/gov/jsasociados/ejb/EmpleadoEJBRemote.java
@@ -0,0 +1,66 @@
+package co.gov.jsasociados.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import co.gov.jsasociados.Recolector;
+import co.gov.jsasocioados.exeption.ElementoRepetidoException;
+import co.gov.jsasocioados.exeption.PersonaNoRegistradaException;
+import co.gov.jsasocioados.exeption.TipoClaseException;
+
+@Remote
+public interface EmpleadoEJBRemote {
+
+	 /**
+     * Metodo para insertar un recolector a la base de datos
+     * @param empleado
+     * @return El recolector a insertar si no ha excepcion
+     * @throws ElementoRepetidoException
+     * @throws PersonaNoRegistradaException
+     */
+	public Recolector insertarRecolector(Recolector recolector)
+			throws ElementoRepetidoException, PersonaNoRegistradaException;
+
+	/**
+     * Metodo para eliminar un recolector por cedula
+     * @param cedula por la cual se busca al recolector
+     * @return True si se elimino correctamente el recolector y False de lo contrario
+     * @throws PersonaNoRegistradaException
+     * @throws TipoClaseException
+     */
+    public boolean eliminarRecolector(String cedula) throws PersonaNoRegistradaException, TipoClaseException;
+    
+    /**
+     * metodo para modificar un recolector
+     * @param nombre
+     * @param apellido
+     * @param telefono
+     * @param correo
+     * @param direccion
+     * @param cedula
+     * @return El recolector modificado
+     * @throws PersonaNoRegistradaException
+     * @throws TipoClaseException
+     */
+    public Recolector modificarRecolector(String nombre, String apellido, String telefono, String correo,
+			String direccion, String cedula) throws PersonaNoRegistradaException, TipoClaseException;
+    
+    /**
+     * Metodo para listar a todos los recolectores
+     * @return La lista de recolectores 
+     * @throws Exception
+     */
+    public List<Recolector> listarRecolectores() throws Exception;
+    
+    /**
+     * Metodo para buscar un recolector por cedula
+     * @param cedula
+     * @return El recolector si fue encontrado o null de lo contrario
+     * @throws PersonaNoRegistradaException
+     * @throws TipoClaseException
+     */
+    public Recolector buscarRecolector(String cedula) throws PersonaNoRegistradaException, TipoClaseException;
+    
+    
+}
