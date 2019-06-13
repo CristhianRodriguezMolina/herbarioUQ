d92e2af5c7aa67cba837bdf51353c4ec43b378f7
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2019-06-13 13:14:35 -0500

    Revert "."
    This reverts commit bb9735fb6555dc9d079520363448372bb094c82c.

--- herbario/escritorio-huq/src/main/java/co/gov/jsasociados/modelo/AdministradorDelegado.java
+++ herbario/escritorio-huq/src/main/java/co/gov/jsasociados/modelo/AdministradorDelegado.java
@@ -29,7 +29,7 @@ public class AdministradorDelegado {
 	/**
 	 * instancia que representa el ejb remoto de administrador
 	 */
-	private AdminEJBRemote adminEJB;
+	private AdminEJB adminEJB;
 	/**
 	 * permite manejar una unica instancia para le manejo de delegados
 	 */
@@ -40,7 +40,7 @@ public class AdministradorDelegado {
 	 */
 	private AdministradorDelegado() {
 		try {
-			adminEJB = (AdminEJBRemote) new InitialContext().lookup(AdminEJBRemote.JNDI);
+			adminEJB = (AdminEJB) new InitialContext().lookup(AdminEJBRemote.JNDI);
 		} catch (NamingException e) {
 			e.printStackTrace();
 		} 
